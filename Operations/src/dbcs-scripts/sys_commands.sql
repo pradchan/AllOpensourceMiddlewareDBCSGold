dc1946a41aab1387f4723188fc72a742e64f70c3
SIVKRISH <SIVKRISH@SIVKRISH-IN.in.oracle.com>
2017-01-31 15:14:46 +0530

    asdf

--- Operations/src/dbcs-scripts/sys_commands.sql
+++ Operations/src/dbcs-scripts/sys_commands.sql
@@ -8,7 +8,7 @@ conn hr_user/welcome1@PDB1
 /
 drop table employees
 /
-create table employees (employee_id VARCHAR(50), first_name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(50), phone_no VARCHAR(20), hire_date DATE, salary DECIMAL(8,2))
+create table employees (employee_id VARCHAR(50) primary key, first_name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(50), phone_no VARCHAR(20), hire_date DATE, salary DECIMAL(8,2))
 /
 insert into employees (employee_id, first_name, last_name, email, phone_no, hire_date, salary) values ('101', 'Siva', 'Krishna', 'siva.krishna@abc.com', '9848098480', to_date('2008-12-01', 'yyyy-mm-dd'), 25000.0)
 /
