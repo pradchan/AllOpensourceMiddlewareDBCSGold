4c0ab822fb8f1c98e85fab596ee225afa5cae273
PRADCHAN.IN <pradeep.chandramouli@oracle.com>
2017-02-24 20:29:58 +0530

    Modified Zone

--- Operations/src/updateappurl.sh
+++ Operations/src/updateappurl.sh
@@ -3,7 +3,7 @@ cloud_domain=$1;
 ACCS_DATACENTER=$2;
 
 if [ "${ACCS_DATACENTER}" == "em2" ]; then
-  ACCS_DATACENTER='europe';
+  ACCS_DATACENTER='em2';
 fi
 
 sed -i 's/IDENTITY_DOMAIN/'$cloud_domain'/' Employee/index.html
